73b2ca5bcfb3dd3e4bf7dece27ee7c1c9b4127a1
Diane Candler <dianedcandler@gmail.com>
2020-07-01 16:39:47 -0500

    added read and create to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,17 @@
+// CRUD create (.post) read (.get) ipdate (.post) delete (.delete)
 const data = require('../db/db.json');
+const fs = require('fs');
 module.exports = function (app) {
 	app.get('/api/notes', function (req, res) {
+		res.json(data);
+	});
+	app.post(function (req, res) {
+		data.push(req.body);
+		// this request is coming from server.js so only need ./ local
+		fs.writeFile('./db/db.json', JSON.stringify(data), function (err) {
+			if (err) throw err;
+		});
+		// pushed into db but need to show on screen
 		res.json(data);
 	});
 };
