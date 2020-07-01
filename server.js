16f28d7b654927df46e936ad5d545ef03b0d9737
Diane Candler <dianedcandler@gmail.com>
2020-07-01 16:02:01 -0500

    added htmlRoutes.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 //define language
 const express = require('express');
 const app = express();
-// connect to
+// connect to Json and Connect PORT and connected Public folder to app
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static("public"));
+// connect to new routes we are creating
+require("./routes/htmlRoutes")(app);
+const PORT = process.env.PORT || 3000;
+app.listen (PORT, function(){
+    console.log("this app is listening on port 3000");
+});
