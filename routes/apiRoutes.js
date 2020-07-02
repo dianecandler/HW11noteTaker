c4f1b9a722046445bbd97f12f5f35f03d80ffabf
Diane Candler <dianedcandler@gmail.com>
2020-07-01 21:46:05 -0500

    update apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,39 @@
 // CRUD create (.post) read (.get) ipdate (.post) delete (.delete)
-const data = require('../db/db.json');
+const db = require('../db/db.json');
 const fs = require('fs');
+
+let id = db.length + 1;
+
 module.exports = function (app) {
-	app.get('/api/notes', function (req, res) {
-		res.json(data);
-	});
 	app.post(function (req, res) {
-		data.push(req.body);
+		console.log(req.body);
+		req.body.id = id++;
+		db.push(req.body);
 		// this request is coming from server.js so only need ./ local
-		fs.writeFile('./db/db.json', JSON.stringify(data), function (err) {
+		fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
 			if (err) throw err;
+			// pushed into db but need to show on screen
+			res.json(db);
 		});
-		// pushed into db but need to show on screen
-		res.json(data);
+	});
+
+	app.get('/api/notes', function (req, res) {
+		res.json(db);
+	});
+
+	app.delete('/api/notes/:id', function (req, res) {
+		var id = parseInt(req.params.id);
+
+		for (var i = 0; i < db.length; i++) {
+			if (db[i].id === id) {
+				db.splice(i, 1);
+			}
+		}
+		console.log(db);
+
+		fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
+			if (err) throw error;
+			res.json(db);
+		});
 	});
 };
